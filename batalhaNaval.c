58f4f5222377f89a7213455f1744da26a37172f7
PetryckBM <petryck-03@hotmail.com>
2025-08-04 23:46:06 +0000

    Fiz as alterações sugeridas pelo professor

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,45 +2,41 @@
 
 #define linha 10
 #define coluna 10
-int main() 
-{
-    int tabuleiro[linha][coluna];
+#define AGUA 0
+#define NAVIO 3
 
-    printf("Tabuleiro Batalha Naval\n");
+int main() {   
+
+int tabuleiro[linha][coluna];  
 
 
-    printf("     A  B  C  D  E  F  G  H  I  J\n");
-    for (int i = 0; i < 10; i++){
-        printf("%2d ", i + 1);
-        for (int j = 0; j < 10; j++){
-           
-            
-            tabuleiro[i][j] = 0;
-            
-             for(int k = 0; k < 3; k++){
-            tabuleiro[k][k] =3;
-        }
-            for(int d = 2; d < 5 ; d++){
-                tabuleiro[d][9-d]= 3;
-            }
-
-            tabuleiro[9][2] =3;
-            tabuleiro[8][2] =3;
-            tabuleiro[7][2] =3;
-
-            tabuleiro[6][7] =3;
-            tabuleiro[6][8] =3;
-            tabuleiro[6][9] =3;
-            
-            
-
-            
-            printf("%3d", tabuleiro[i][j]);
-        }
-    
-        printf("\n");
-    }
-    
-
-    return 0;
+for (int i = 0; i < linha; i++){ 
+    for (int j = 0; j < coluna; j++){  
+    tabuleiro[i][j] = AGUA;} 
 }
+for(int k = 0; k < 3; k++){        
+    tabuleiro[k][k] = NAVIO;}
+
+for(int d = 2; d < 5 ; d++){       
+     tabuleiro[d][9-d]= NAVIO;}
+ 
+     tabuleiro[9][2] = NAVIO;    
+     tabuleiro[8][2] = NAVIO;    
+     tabuleiro[7][2] = NAVIO;    
+     tabuleiro[6][7] = NAVIO;    
+     tabuleiro[6][8] = NAVIO;    
+     tabuleiro[6][9] = NAVIO;    
+
+printf("Tabuleiro Batalha Naval\n");
+
+printf("     A  B  C  D  E  F  G  H  I  J\n");    
+     for (int i = 0; i < linha; i++){        
+        printf("%2d ", i + 1);       
+        for (int j = 0; j < coluna; j++){            
+            printf("%3d", tabuleiro[i][j]);        
+        }       
+         printf("\n");    
+        }       
+        
+        return 0;
+    }
