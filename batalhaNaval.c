c1f13d1b9823c14558c1a12ac2d581f4f12cb40f
PetryckBM <petryck-03@hotmail.com>
2025-08-06 16:16:04 +0000

    Fiz os ajustes sugeridos e nao consegui fazer o ataque em cone e em Octaedro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -8,13 +8,14 @@
 int main() {   
 
 int tabuleiro[linha][coluna];
-int habilidade [5][5]; 
+ 
 
-
+// Implementação do tabuleiro 
 for (int i = 0; i < linha; i++){ 
     for (int j = 0; j < coluna; j++){  
     tabuleiro[i][j] = AGUA;} 
 }
+//implementação de alguns navios
 for(int k = 0; k < 3; k++){        
     tabuleiro[k][k] = NAVIO;}
 
@@ -27,14 +28,12 @@ for(int i = 7; i < 10; i++){
  for(int i = 7; i < 10; i++){
      tabuleiro[6][i] = NAVIO; 
 }        
-       
-for(int i =3; i < 8; i++ ){
-    tabuleiro[i][5] = ACERTO;
-    for(int j = 3; j < 8; j++){
-        
-            tabuleiro[5][j] = ACERTO;
-        
-    }
+// implementação de um ataque em cruz       
+for(int i =3; i < 8; i++ ){    
+    tabuleiro[i][5] = ACERTO;    
+    for(int j = 3; j < 8; j++){        
+    tabuleiro[5][j] = ACERTO;    
+}
 }
 
 
